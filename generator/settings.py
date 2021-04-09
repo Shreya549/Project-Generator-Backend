8bf60b03cc2d8ec718428587a5c96f78a5d9ecaf
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2021-04-09 19:11:03 +0530

    fix: minor changes

--- generator/settings.py
+++ generator/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os, environ
+import os, environ, django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env(
@@ -159,4 +159,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ] 
+
+django_heroku.settings(locals())
 
