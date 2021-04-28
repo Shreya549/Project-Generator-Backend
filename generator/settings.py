ead75ffbe26ceb65b5e29d2df29b0b23938a24de
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2021-04-28 12:15:01 +0530

    Parsers added

--- generator/settings.py
+++ generator/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'generator.urls'
 
+CORS_ALLOWED_ORIGINS = ['']
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'Accounts.backend.JWTFacultyAuthentication',
@@ -77,6 +79,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PARSER_CLASSES':[
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
