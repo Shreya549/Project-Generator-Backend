978a1613c6595056a43b3dd5763df0139095db18
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2021-05-02 23:35:42 +0530

    fix: invalid password exception

--- Accounts/views.py
+++ Accounts/views.py
@@ -53,7 +53,7 @@ class StudentRegistration(APIView):
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
 
-        serializer.is_valid()
+        serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
