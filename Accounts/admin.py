f77b945dd9a84bde0aecb8755a5157a5cf4fec4f
Shreya Chatterjee <43725310+Shreya549@users.noreply.github.com>
2021-06-01 15:17:52 +0530

    feat: Contact table added to admin

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Faculty, Student
+from .models import User, Faculty, Student, Contact
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Faculty)
 admin.site.register(Student)
+admin.site.register(Contact)
